81d0cc852fb68be8d236c625d0a618b6fd3e427e
priyankap12308 <priyankap12308@gmail.com>
2016-12-01 15:37:59 -0800

    Programs

---
+++ TestClass3.java
@@ -0,0 +1,11 @@
+package Programs;
+
+public class TestClass3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s="abcaaabbbbbbbbcccccaaaa";
+		System.out.println(s.replace("a", ""));
+	}
+}
